563d433502608f9ee97532a6aefc43be0834bf7e
Anjana Vakil <contact@anjana.dev>
2024-12-11 20:01:32 -0600

    Update import paths & enable .ts imports in config

--- event-me/src/main.js
+++ event-me/src/main.js
@@ -1,9 +1,17 @@
 import './style.css'
+
+// Importing explicit .js files should Just Work,
+// even if we've renamed them to .ts (it's all .js at runtime)
 import Header, { setupThemeToggle } from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
-import { setupModals } from './components/Modal.js';
-import { setupForms } from './components/Forms.js';
+
+// Importing explicit .ts files requires
+// tsconfig setting  "allowImportingTsExtensions": true
+import { setupModals } from './components/Modal.ts';
+
+// Importing without an explicit extension should Just Work
+import { setupForms } from './components/Forms';
 
 const { VITE_API_URL } = import.meta.env;
 if (!VITE_API_URL) throw new Error('Missing env var VITE_API_URL');
