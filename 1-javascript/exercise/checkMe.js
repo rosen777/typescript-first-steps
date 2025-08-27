b649e26c191c3d166fa95ea61083756b732f2b49
Rosen Toshev <rtoscheff@gmail.com>
2025-08-27 16:04:42 +0100

    TS exercises

--- 1-javascript/exercise/checkMe.js
+++ 1-javascript/exercise/checkMe.js
@@ -1,26 +1,23 @@
-
 let language = {
-    name: 'JavaScript',
-    officialName: 'ECMAScript',
-    released: 1995,
-    creator: 'Brendan Eich',
-    company: 'Netscape'
-}
+  name: "JavaScript",
+  officialName: "ECMAScript",
+  released: 1995,
+  creator: "Brendan Eich",
+  company: "Netscape",
+};
 
 console.log(language); // { name: 'JavaScript', ... }
 console.log(language.officialName); // ECMAScript
-console.log(language.creater); // undefined
+console.log(language.creator); // undefined
 
-
-language = 'TypeScript'; // String replacing object? JS don't care!
+language.name = "TypeScript"; // String replacing object? JS don't care!
 // Setting random properties on a String? (non-strict) JS don't care!
 language.released = 2012;
-language.creator = 'Anders Hejlsberg';
-language.company = 'Microsoft';
+language.creator = "Anders Hejlsberg";
+language.company = "Microsoft";
+language.officialName = language.name;
 
 console.log(language); // TypeScript
 console.log(language.released); // undefined
-console.lgo(language.company); // TypeError: console.lgo is not a function
+console.log(language.company); // TypeError: console.lgo is not a function
 console.log(language.officialName); // undefined
-
-
